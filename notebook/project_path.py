9d470a4dd3b39a33f33468574ac5d1557bcf6ce8
nilesh <nilesh@nileshdev>
2019-11-06 23:51:29 +0000

    update to ML notebook

--- notebook/project_path.py
+++ notebook/project_path.py
@@ -8,11 +8,6 @@ import sys
 import os
 
 module_path = os.path.join(os.path.dirname(os.path.dirname(str(__file__))), "src")
-resource_path = os.path.join(os.path.dirname(os.path.dirname(str(__file__))), "data")
-config_path = os.path.join(os.path.dirname(os.path.dirname(str(__file__))), "conf")
 
-_path = [module_path, resource_path, config_path]
-
-for p in _path:
-    if p not in sys.path:
-        sys.path.append(p)
+if module_path not in sys.path:
+    sys.path.append(module_path)
