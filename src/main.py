cecee6d0c754fff60218ef96023ebfd2dfe43929
nilesh <nilesh@nileshdev>
2019-11-16 14:31:28 +0000

    refactoring of classifier module

--- src/main.py
+++ src/main.py
@@ -44,36 +44,35 @@ if __name__ == "__main__":
     log = logging.getLogger("cqf_logger")
     log.info("Initialising Program For CQF Exam 3 Machine Learning with Python")
     try:
-        #define datamodels here
+        #Create datamodel here
         dax_data = DataModel(filename="dax.csv")
-        es_50_data = DataModel(filename="eurostoxx.csv")
-         
+        estoxx_data = DataModel(filename="eurostoxx.csv")
+        
+        #Create Logistic Regression Classifier Here
         dax_logit = LogisticalRegression()
         estoxx_logit = LogisticalRegression()
         
+        #Create SVM Classifier Here
+        dax_svm = SupportVectorMachine()
+        estoxx_svm = SupportVectorMachine()
+        
         #Run classifier
-        dax_logit.run_classifier(dax_data)
-        estoxx_logit.run_classifier(es_50_data)
+        dax_svm.run_classifier(dax_data)
+        estoxx_svm.run_classifier(estoxx_data)
+#         dax_logit.run_classifier(dax_data)
+#         estoxx_logit.run_classifier(estoxx_data)
 #         pl = PLBacktestingEngine()
 #         pl.run_backtest(data=dax_data, initial_capital=500000, bet_size=0.1, 
 #                         upper_bound=0.70, lower_bound=0.55)
-#         pl.run_backtest(data=es_50_data, initial_capital=500000, bet_size=0.1, 
+#         pl.run_backtest(data=estoxx_data, initial_capital=500000, bet_size=0.1, 
 #                         upper_bound=0.70, lower_bound=0.55)
-        
-#         support_vector_machine = SupportVectorMachine()
-#         support_vector_machine.run_classifier(dax_data))
-#          
-        g = GraphLib()
-#         g.plot_multimodel_pl_backtest(dax_data, es_50_data)
-#         g.plot_transition_probabilities_multi_model(dax_data, es_50_data)
-#         g.plot_multimodel_roc_curve(dax_data, es_50_data)
-        g.plot_multimodel_confusion_matrix(dax_data, es_50_data, ["Positive Returns", "Negative Returns"], 
-                                           "Confusion Matrix - Logistic Regression")
-#         g.plot_roc_curve(dax_data)
-#         g.plot_returns(data)
-#         g.plot_confusion_matrix(dax_data.c_matrix, ["Positive Returns", "Negative Returns"],
-#                                 "Confusion Matrix - Logistic Regression")
 
+#         g = GraphLib()
+#         g.plot_multimodel_pl_backtest(dax_data, estoxx_data)
+#         g.plot_transition_probabilities_multi_model(dax_data, estoxx_data)
+#         g.plot_multimodel_roc_curve(dax_data, estoxx_data)
+#         g.plot_multimodel_confusion_matrix(dax_data, estoxx_data, ["Positive Returns", "Negative Returns"], 
+#                                            "Confusion Matrix - Logistic Regression")
     except Exception as e:
         print(e)
 
--- src/ml/classifiers.py
+++ src/ml/classifiers.py
@@ -42,6 +42,25 @@ class Classification(metaclass=ABCMeta):
          
         self._logger.info("Finished test train data with split ratio: Training data {} Test data {} ".format(1 - size, size))
 
+    def compute_confusion_matrix_test_sample(self, data, classifier):
+        self.fit_model(data.x_train, data.y_train, classifier)
+        y_pred_test = classifier.predict(data.x_test)
+        self._logger.info("Confusion matrix as follows:")
+        data.c_matrix_test = confusion_matrix(data.y_test, y_pred_test)
+        self._logger.info(data.c_matrix_test)
+        #pretty print confusion matrix with data labels
+        c_matrix = self._pretty_print_confusion_matrix(data.y_test, y_pred_test)
+        return c_matrix
+
+    def compute_test_data_roc_metrics(self, data=None):
+        data.fpr_test, data.tpr_test, data.thresholds_test = roc_curve(data.y_test, data.y_pred_test)
+        data.roc_auc_score_test = roc_auc_score(data.y_test, data.y_pred_test)
+        self._logger.info("ROC AUC Test Data Score: {}".format(data.roc_auc_score_test))
+
+    def compute_test_data_predicted_probabilities(self, data=None, classifier=None):
+        self._logger.info("Computing predicted probabilities on test set of data")
+        data.y_pred_prob_test = classifier.predict_proba(data.x_test)[:, 1]
+
     def _pretty_print_confusion_matrix(self, y_test, y_pred):
         unique_label = np.unique([y_test, y_pred])
         c_matrix = pd.DataFrame(
@@ -51,17 +70,49 @@ class Classification(metaclass=ABCMeta):
         )
         self._logger.info(c_matrix)
         return c_matrix
+
+    def apply_gridsearch(self, data=None, classifier=None):
+        self._grid = GridSearchCV(classifier, self._params, verbose=3)
+        self._grid.fit(data.x_train, data.y_train)
+        self._logger.info("Best params to use for chosen classifier: {}".format(self._grid.best_params_))
+        grid_predictions = self._grid.predict(data.x_test)
+        self._logger.info("Confusion Matrix as Follows: {}".format(confusion_matrix(data.y_test, grid_predictions)))
+        self._logger.info("Classification Report: {}".format(classification_report(data.y_test, grid_predictions)))
+
+    def get_predicted_probabilities(self, data=None, x_features=None, classifier=None):
+        data.pred_prob = classifier.predict_proba(data.model[x_features])
+        self._logger.info("Computed predicted probabilities")
+
+    def _run_main_classifier(self, data=None, x_features=None, y_result=None, classifier=None):
+        self._logger.info("Fitting model against specified parameters")
+        self.fit_model(data.model[x_features], y_result, classifier)
+        self.run_prediction(data, x_features, classifier)
+        self.compute_confusion_matrix_main(data)
+        self.compute_population_data_roc_metrics(data)
+        self.compute_population_data_predicted_probabilities(data=data, x_features=x_features, classifier=classifier)
+
+    def _run_test_fit_classifier(self, data=None, x_features=None, y_result=None, classifier=None, size=None):
+        self.train_test_split(data=data, features=x_features, size=size, test_param=y_result)
+        self.fit_model(data.x_train, data.y_train, classifier)
+        data.y_pred_test = classifier.predict(data.x_test)
+        self.compute_test_data_roc_metrics(data=data)
+        self.compute_test_data_predicted_probabilities(data=data, classifier=classifier)
     
 class LogisticalRegression(Classification):
     
     def __init__(self):
         super(LogisticalRegression, self).__init__()
         self._init_classifier()
-
+        self._init_grid_search_params()
+        
     def _init_classifier(self):
         self.logreg_main = linear_model.LogisticRegression(penalty="l2", C = 100)
         self.logreg_test = linear_model.LogisticRegression(penalty="l2", C = 100)
     
+    def _init_grid_search_params(self):
+        self._params = {"C" : [100000, 10000, 1000, 100, 10, 1, .1, .001],
+                        "penalty" : ["l1", "l2"]}
+        
     def fit_model(self, x_param, y_param, classifier):
         classifier.fit(x_param, y_param)
 
@@ -76,90 +127,35 @@ class LogisticalRegression(Classification):
         #Separate Data Parameters of x_features and y_response
         returns_sign = data.model.log_return_sign
         lagged_headers = [header for header in list(data.model) if "lagged" in header]
-        
         #Run main classifier
         self._run_main_classifier(data=data, x_features=lagged_headers, 
                                   y_result=returns_sign, classifier=self.logreg_main)
-        
         #Apply train test split on classifier
         self._run_test_fit_classifier(data=data, x_features=lagged_headers, 
                                       y_result=returns_sign, classifier=self.logreg_test, size=0.25)
-        
         #Perform optimal gridsearch for parameters here
-#         self.apply_gridsearch(data)
+#         self.apply_gridsearch(data=data, classifier=linear_model.LogisticRegression())
 #         
-        self.get_predicted_probabilities(data, lagged_headers, self.logreg_main)
-#         
+        self.get_predicted_probabilities(data=data, x_features=lagged_headers, classifier=self.logreg_main)
         self._logger.info("Finished running through classifier")
-
-    def _run_main_classifier(self, data=None, x_features=None, y_result=None, classifier=None):
-        self._logger.info("Fitting model against specified parameters")
-        self.fit_model(data.model[x_features], y_result, classifier)
-        self.run_prediction(data, x_features, classifier)
-        self.compute_confusion_matrix_main(data)
-        self.compute_population_data_roc_metrics(data)
-        self.compute_population_data_predicted_probabilities(data, x_features, classifier)
-    
-    def _run_test_fit_classifier(self, data=None, x_features=None, y_result=None, classifier=None, size=None):
-        self.train_test_split(data=data, features=x_features, size=size, test_param=y_result)
-        self.fit_model(data.x_train, data.y_train, classifier)
-        data.y_pred_test = classifier.predict(data.x_test)
-        self.compute_test_data_roc_metrics(data)
-        self.compute_test_data_predicted_probabilities(data, classifier)
-        
-    def compute_confusion_matrix_test_sample(self, data, classifier):
-        self.fit_model(data.x_train, data.y_train, classifier)
-        y_pred_test = classifier.predict(data.x_test)
-        self._logger.info("Confusion matrix as follows:")
-        data.c_matrix_test = confusion_matrix(data.y_test, y_pred_test)
-        self._logger.info(data.c_matrix_test)
-        
-        #pretty print confusion matrix with data labels
-        c_matrix = self._pretty_print_confusion_matrix(data.y_test, y_pred_test)
-        return c_matrix
 
     def compute_confusion_matrix_main(self, data):
         self._logger.info("Computing Confusion matrix for main data as follows:")
         data.c_matrix_main = confusion_matrix(data.model.log_return_sign, data.model.log_pred)
         self._logger.info(data.c_matrix_main)
-        
         #pretty print confusion matrix with data labels
         c_matrix = self._pretty_print_confusion_matrix(data.model.log_return_sign, data.model.log_pred)
         return c_matrix
-
-    def compute_test_data_roc_metrics(self, data):
-        data.fpr_test, data.tpr_test, data.thresholds_test = roc_curve(data.y_test, data.y_pred_test)
-        data.roc_auc_score_test = roc_auc_score(data.y_test, data.y_pred_test)
-        self._logger.info("ROC AUC Test Data Score: {}".format(data.roc_auc_score_test))
 
     def compute_population_data_roc_metrics(self, data):
         data.fpr_main, data.tpr_main, data.thresholds_main = roc_curve(data.model.log_return_sign, data.model.log_pred)
         data.roc_auc_score_main = roc_auc_score(data.model.log_return_sign, data.model.log_pred)
         self._logger.info("ROC AUC Main Data Score: {}".format(data.roc_auc_score_main))
-    
-    def compute_test_data_predicted_probabilities(self, data, classifier):
-        self._logger.info("Computing predicted probabilities on test set of data")
-        data.y_pred_prob_test = classifier.predict_proba(data.x_test)[:, 1]
 
-    def compute_population_data_predicted_probabilities(self, data, features, classifier):
+    def compute_population_data_predicted_probabilities(self, data=None, x_features=None, classifier=None):
         self._logger.info("Computing predicted probabilities on population data")
-        data.y_pred_prob_population = classifier.predict_proba(data.model[features])[:, 1]
+        data.y_pred_prob_population = classifier.predict_proba(data.model[x_features])[:, 1]
         
-    def get_predicted_probabilities(self, data, x_features, classifier):
-        data.pred_prob = classifier.predict_proba(data.model[x_features])
-        self._logger.info("Computed predicted probabilities")
-    
-    def apply_gridsearch(self, data):
-        self._params = {"C" : [100000, 10000, 1000, 100, 10, 1, .1, .001],
-                        "penalty" : ["l1", "l2"]}
-        self._grid = GridSearchCV(linear_model.LogisticRegression(),
-                                  self._params, verbose=3)
-        self._grid.fit(data.x_train, data.y_train)
-        self._logger.info("Best params to use for chosen classifier: {}".format(self._grid.best_params_))
-        grid_predictions = self._grid.predict(data.x_test)
-        self._logger.info("Confusion Matrix as Follows: {}".format(confusion_matrix(data.y_test, grid_predictions)))
-        self._logger.info("Classification Report: {}".format(classification_report(data.y_test, grid_predictions)))
-
     def test_regularisation_strengths(self, x_train, y_train, x_test, y_test):
         strengths = [100000, 10000, 1000, 100, 10, 1, .1, .001]
         #Create a scaler object
@@ -168,7 +164,7 @@ class LogisticalRegression(Classification):
         x_train_std = sc.fit_transform(x_train)
         #Apply the scaler to the test data
         x_test_std = sc.transform(x_test)
-        
+
         for c in strengths:
             clf = linear_model.LogisticRegression(penalty='l1', C=c) #solver='liblinear'
             clf.fit(x_train, y_train)
@@ -184,55 +180,57 @@ class SupportVectorMachine(Classification):
         self._init_classifier()
 
     def _init_classifier(self):
-        self.svm = svm.SVC(C = 1e5, probability=True)
-    
-    def fit_model(self, x_param, y_param):
-        self.svm.fit(x_param, y_param)
+        self.svm_main = svm.SVC(C = 1e5, probability=True)
+        self.svm_test = svm.SVC(C = 1e5, probability=True)
+
+    def _init_grid_search_params(self):
+        self._params = {"C" : [100000, 10000, 1000, 100, 10, 1, .1, .001]}
         
-    def run_classifier(self, data):
-        self._logger.info("Running SVM Classifier")
-        #perform the regression on returns here
-        returns_sign = data.model.log_return_sign
-        lagged_headers = [header for header in list(data.model) if "lagged" in header]
-        #fitting done here
-        self._logger.info("Fitting model against specified parameters")
-        self.fit_model(data.model[lagged_headers], returns_sign)
-        self.run_prediction(data, lagged_headers)
-        pred_prob = self.svm.predict_proba(data.model[lagged_headers])
-        print("max min transition probabilities :")
-        print(max(pred_prob[:,1]))
-        print(min(pred_prob[:,1]))
-        data.model["svm_return"] = abs(data.model.log_return) * data.model.svm_pred
-        print("SVM score: {}".format(self.svm.score(data.model[lagged_headers], returns_sign)))
+    def fit_model(self, x_param, y_param, classifier):
+        classifier.fit(x_param, y_param)
         
-        self.train_test_split(data=data, features=lagged_headers, size=0.7, test_param=returns_sign)
-        
-    def run_prediction(self, data, x_features):
+    def run_prediction(self, data, x_features, classifier):
         self._logger.info("Running SVM Prediction")
-        data.model["svm_pred"] = self.svm.predict(data.model[x_features])
+        data.model["svm_pred"] = classifier.predict(data.model[x_features])
+        self._logger.info("Actual Return Summary: {}".format(data.model.log_return_sign.value_counts()))
+        self._logger.info("SVM Prediction Summary: {}".format(data.model.svm_pred.value_counts()))
+    
+    def run_classifier(self, data):
+        self._logger.info("Running SVM Classifier for {}".format(data.filename))
+        #Separate Data Parameters of x_features and y_response
+        returns_sign = data.model.log_return_sign
+        x_features = [header for header in list(data.model) if "lagged" in header]
+        
+        #Run main classifier
+        self._run_main_classifier(data=data, x_features=x_features, 
+                                  y_result=returns_sign, classifier=self.svm_main)
+        
+        #Apply train test split on classifier
+        self._run_test_fit_classifier(data=data, x_features=x_features, 
+                                      y_result=returns_sign, classifier=self.svm_test, size=0.25)
+        
+        #Perform optimal gridsearch for parameters here
+        self.apply_gridsearch(data=data)
+        self.get_predicted_probabilities(data=data, x_features=x_features, classifier=self.svm_main)
+        self._logger.info("Finished running through classifier")
+        
+    def compute_confusion_matrix_main(self, data):
+        self._logger.info("Computing Confusion matrix for main data as follows:")
+        data.c_matrix_main = confusion_matrix(data.model.log_return_sign, data.model.svm_pred)
+        self._logger.info(data.c_matrix_main)
+        #pretty print confusion matrix with data labels
+        c_matrix = self._pretty_print_confusion_matrix(data.model.log_return_sign, data.model.svm_pred)
+        return c_matrix
 
-    def train_test_split(self, data=None, features=None, size=None, test_param=None):
-        #default split to half the data set
-        if size is None:
-            size = 0.5
-        self.x_train, self.x_test, self.y_train, self.y_test = model_selection.train_test_split(data.model[features], test_param,
-                                                                            test_size=size, shuffle=False)
-        print("Running second svm classifier on training set")
-        svm2 = svm.SVC(C = 1e5, probability=True)
-        svm2.fit(self.x_train, self.y_train)
-        y_pred = svm2.predict(self.x_test)
-        print("Confusion matrix as follows:")
-        #first arguments to confusion matrix is true values and the second is predicted vals
-        self.c_matrix = confusion_matrix(self.y_test, y_pred)
-        print(self.c_matrix)
+    def compute_population_data_roc_metrics(self, data):
+        data.fpr_main, data.tpr_main, data.thresholds_main = roc_curve(data.model.log_return_sign, data.model.svm_pred)
+        data.roc_auc_score_main = roc_auc_score(data.model.log_return_sign, data.model.svm_pred)
+        self._logger.info("ROC AUC Main Data Score: {}".format(data.roc_auc_score_main))
 
-        unique_label = np.unique([self.y_test, y_pred])
-        cmtx = pd.DataFrame(
-            confusion_matrix(self.y_test, y_pred, labels=unique_label), 
-            index=['true:{:}'.format(x) for x in unique_label], 
-            columns=['pred:{:}'.format(x) for x in unique_label]
-        )
-        print(cmtx)
+    def compute_population_data_predicted_probabilities(self, data, features, classifier):
+        self._logger.info("Computing predicted probabilities on population data")
+        data.y_pred_prob_population = classifier.predict_proba(data.model[features])[:, 1]
+        
     
 class ANN(Classification):
     
@@ -242,10 +240,3 @@ class ANN(Classification):
     
     def run_classifier(self):
         pass
-
-    def train_test_split(self, data=None, features=None, size=None, test_param=None):
-        #default split to half the data set
-        if size is None:
-            size = 0.5
-        self.x_train, self.x_test, self.y_train, self.y_test = model_selection.train_test_split(data.model[features], test_param,
-                                                                            test_size=size, shuffle=False)
